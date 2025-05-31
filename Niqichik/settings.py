c36afc9b6b86e357be561250c3191ef75df3feff
unknown <david.valenzuela.oyaneder@gmail.com>
2025-05-31 00:54:00 -0400

    Mensaje del commit

--- Niqichik/settings.py
+++ Niqichik/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6lo!@abc0_ephg0ohwtjv6!mta+_x+hj)7wy1%-i4q5m=92l85'
+from decouple import config
+
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
