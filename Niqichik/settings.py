5c7dff06428598ae84871feb2485e444e1e97b9c
unknown <david.valenzuela.oyaneder@gmail.com>
2025-05-31 01:04:25 -0400

    Mensaje del commit

--- Niqichik/settings.py
+++ Niqichik/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
 
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
@@ -29,8 +29,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['niqichik.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
