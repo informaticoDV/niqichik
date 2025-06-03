1641fad52f4c31eadb7b9e6ad4ba8ede350a63cd
unknown <david.valenzuela.oyaneder@gmail.com>
2025-06-03 16:40:40 -0400

    Se añade nuevo link www.niqichik.com

--- Niqichik/settings.py
+++ Niqichik/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['niqichik.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['niqichik.onrender.com', 'localhost', '127.0.0.1','www.niqichik.com']
 
 # Application definition
 
