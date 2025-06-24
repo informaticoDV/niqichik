d5e8f12a436c2a08d59526b609baf172cf4df8c7
unknown <david.valenzuela.oyaneder@gmail.com>
2025-06-24 17:03:19 -0400

    Se añade STATICFILES_DIRS en settings

--- Niqichik/settings.py
+++ Niqichik/settings.py
@@ -152,7 +152,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
