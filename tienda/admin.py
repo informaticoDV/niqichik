0c388ba3f84415ebb3ab9e7087fae6476c50f0da
informaticoDV <54446312+informaticoDV@users.noreply.github.com>
2025-07-01 00:06:21 -0400

    Update admin.py
    Se añade Venta

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Producto, Pedido, Informacion, Categoria
+from .models import Producto, Pedido, Informacion, Categoria, Venta
 
 admin.site.register(Producto)
 admin.site.register(Pedido)
 admin.site.register(Informacion)
 admin.site.register(Categoria)
+admin.site.register(Venta)
